2c23b0e5bcd5d539e6c042e0d6cb2a472f7e62d7
Adil Hussain <adilh621@gmail.com>
2020-10-10 20:27:25 -0400

    markdown function completed to take data and write it to .md file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,36 @@
-function generateMarkdown(data, githubInfo) {
-  
+function generateMarkdown(data, github) {
+  return `
+# **${data.title}**
+
+## Description 
+${data.description}
+## Table of contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+## Installation
+        ${data.installation}
+## Usage
+${data.usage}
+## Licence
+${data.licence}
+## Contributors
+${data.contributing}
+## Test
+${data.test}
+## Repository
+- [Project Repo](${data.repo})
+## GitHub
+![Image of me](${github.githubImage})
+- ${github.name}
+- [GitHub Profile](${github.profile})
+- <${github.email}>
+`;
 }
 
 module.exports = generateMarkdown;
